e4d9625744267317a90842b49ff5a6c81c2be3ed
EvgenijDev <j238267431@mail.ru>
2025-11-13 14:40:05 +0300

    Update App.js

--- web/src/App.js
+++ web/src/App.js
@@ -35,7 +35,7 @@ function App() {
   }, []);
 
   const register = () => {
-    if (!inputName.trim()) return alert("Введите имя 44!");
+    if (!inputName.trim()) return alert("Введите имя 445!");
     socket.emit("register", { name: inputName.trim(), password: FAMILY_PASSWORD });
   };
 
