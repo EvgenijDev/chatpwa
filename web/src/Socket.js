97bcd8e55721f3f0875d1a3afa86d98f9f8844f4
EvgenijDev <j238267431@mail.ru>
2025-12-13 18:22:33 +0300

    Update Socket.js

--- web/src/Socket.js
+++ web/src/Socket.js
@@ -1,5 +1,6 @@
 import { io } from "socket.io-client";
 
+console.log('REACT_APP_SERVER_URL', process.env.REACT_APP_SERVER_URL);
 const socket = io(process.env.REACT_APP_SERVER_URL || "https://chatpwa.ru", {
    transports: ["websocket"],
    path: "/socket.io" 
