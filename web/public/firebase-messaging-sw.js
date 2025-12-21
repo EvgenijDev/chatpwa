185601c505558ce0c1481655fb5864fd4a614d94
EvgenijDev <j238267431@mail.ru>
2025-12-21 21:22:48 +0300

    firebase config

--- web/public/firebase-messaging-sw.js
+++ web/public/firebase-messaging-sw.js
@@ -1,15 +1,9 @@
 // Firebase SW
+importScripts("/firebase-config.js");
 importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js");
 importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js");
 
-firebase.initializeApp({
-   apiKey: process.env.REACT_APP_API_KEY,
-   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-   projectId: process.env.REACT_APP_PROJECT_ID,
-   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-   appId: process.env.REACT_APP_APP_ID,
-});
+firebase.initializeApp(self.FIREBASE_CONFIG);
 
 const messaging = firebase.messaging();
 
