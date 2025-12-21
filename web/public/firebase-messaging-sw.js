4a14fc1e97b165d81e8fb0c1be53a246151b7cbc
EvgenijDev <j238267431@mail.ru>
2025-12-21 21:28:10 +0300

    Update firebase-messaging-sw.js

--- web/public/firebase-messaging-sw.js
+++ web/public/firebase-messaging-sw.js
@@ -1,5 +1,5 @@
 // Firebase SW
-importScripts("/firebase-config.js");
+importScripts("/public/firebase-config.js");
 importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js");
 importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js");
 
