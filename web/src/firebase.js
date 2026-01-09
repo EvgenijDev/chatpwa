b7d841068dcd08a048e98cbb31e204bf8590c7fb
EvgenijDev <j238267431@mail.ru>
2026-01-09 16:46:50 +0300

    Update firebase.js

--- web/src/firebase.js
+++ web/src/firebase.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 import { getMessaging, getToken, onMessage } from "firebase/messaging";
 
 const firebaseConfig = {
